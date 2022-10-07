7dd208dea76fc8a64ce9ad407c0ffab4ceb1067a
William O'Hanley <me@wohanley.com>
2022-10-07 14:19:50 -0700

    Epilog: Quote entity names in attribute relations

--- utils/toEpilog.js
+++ utils/toEpilog.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const loadObjFromSrc  = require('./lib/loadObjFromSrc.js');
+const injectInheritedProperties  = require('./lib/injectInheritedProperties.js');
 
 const DATA_MODEL_SRC_PATH = `${__dirname}/../datamodel-src`;
 const DATA_MODEL_EPILOG_PATH = `${__dirname}/../datamodel-build/epilog`;
@@ -54,7 +55,7 @@ function renderObject(name, data) {
     const prop = data.properties[propName];
     const relationName = `"${name}.${propName}"`;
 
-    output += `\nattribute(${name},${relationName})`;
+    output += `\nattribute("${name}",${relationName})`;
 
     output += `\ntype(${relationName},attributerelation)`;
 
